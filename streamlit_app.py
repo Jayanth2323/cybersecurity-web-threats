977b6c02429e796ecd2c3654ee7916f7359d3df5
Jayanth2323 <jayanthchennoju@gmail.com>
2025-04-23 21:25:10 +0530

    minor changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -134,6 +134,19 @@ packet_range = st.slider(
     ),
 )
 
+# Sidebar - dynamic field filters
+fields = ["protocol", "dest_ip_country_code"]
+some_options = {
+    field: df[field].dropna().unique().tolist() for field in fields}
+selected_values = {}
+
+for i, field in enumerate(fields):
+    selected_values[field] = st.sidebar.multiselect(
+        f"Select {field} Options",
+        options=some_options[field],
+        key=f"multiselect_{field}_{i}"
+    )
+
 # Filtering
 filtered_df = df[
     df["anomaly"].isin(status)
